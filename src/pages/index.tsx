dc864bffc84bd79ade2520c8135ee3ae229a1682
Matheus Pimentel <matheus08.pimentel@gmail.com>
2021-07-05 13:40:53 -0300

    Create the boiler plate

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container } from '../styles/pages/Home';
+
+export default function Home() {
+  return (
+    <Container>
+      <h1>Hello :)</h1>
+      <p>Let's start to develop our Next.js app</p>
+    </Container>
+  );
+}
